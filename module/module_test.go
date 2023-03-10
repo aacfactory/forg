939efa3537937c8619bf341d3032b5615292e6b5
wangminxiang <ryougi.nevermore@hotmail.com>
2023-03-10 15:45:00 +0800

    add test fn

--- module/module_test.go
+++ module/module_test.go
@@ -1,6 +1,7 @@
 package module_test
 
 import (
+	"context"
 	"fmt"
 	"github.com/aacfactory/forg/module"
 	"testing"
@@ -15,4 +16,35 @@ func TestParseModule(t *testing.T) {
 	}
 	fmt.Println(mod)
 
+}
+
+func TestFunction_Parse(t *testing.T) {
+	path := `D:\studio\workspace\go\src\github.com\aacfactory\fns-example\standalone\go.mod`
+	mod, modErr := module.New(path)
+	if modErr != nil {
+		t.Errorf("%+v", modErr)
+		return
+	}
+	fmt.Println(mod.String())
+	services, servicesErr := mod.Services()
+	if servicesErr != nil {
+		t.Errorf("%+v", servicesErr)
+		return
+	}
+	fmt.Println("services:", len(services))
+	if len(services) == 0 {
+		return
+	}
+	service := services[0]
+	fmt.Println("service:", service.Name, "functions:", len(service.Functions), "components:", len(service.Components))
+	if len(service.Functions) == 0 {
+		return
+	}
+	fn := service.Functions[0]
+	parseErr := fn.Parse(context.TODO())
+	if parseErr != nil {
+		t.Errorf("%+v", parseErr)
+		return
+	}
+	fmt.Println("fn:", fn.Name(), fn.Param.String(), fn.Result.String())
 }
