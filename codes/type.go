79ab65745e9d2a1898566631e22508aae0042a85
wangminxiang <ryougi.nevermore@hotmail.com>
2023-03-16 01:17:34 +0800

    mod

--- codes/type.go
+++ codes/type.go
@@ -13,26 +13,29 @@ import (
 
 func MapTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
 	if ctx.Err() != nil {
-		err = errors.Warning("forg: map type to function document element code, failed").
+		err = errors.Warning("forg: mapping type to function document element code, failed").
 			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
 			WithCause(ctx.Err())
+		return
 	}
-
 	switch typ.Kind {
 	case module.BasicKind:
 		code, err = mapBasicTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.BuiltinKind:
-
+		code, err = mapBuiltinTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.IdentKind:
-
+		code, err = mapIdentTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.InterfaceKind:
-
+		code, err = mapInterfaceTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.StructKind:
 		code, err = mapStructTypeToFunctionElementCode(ctx, typ)
+		break
+	case module.StructFieldKind:
+		code, err = mapStructFieldTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.PointerKind:
 		code, err = mapPointerTypeToFunctionElementCode(ctx, typ)
@@ -41,27 +44,32 @@ func MapTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code g
 		code, err = mapArrayTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.MapKind:
-
+		code, err = mapMapTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.AnyKind:
-
+		code, err = mapAnyTypeToFunctionElementCode(ctx, typ)
+		break
 	case module.ParadigmKind:
-		// todo packed
+		code, err = mapParadigmTypeToFunctionElementCode(ctx, typ)
 		break
 	case module.ParadigmElementKind:
-		// todo packed
+		code, err = mapParadigmElementTypeToFunctionElementCode(ctx, typ)
 		break
 	default:
-
+		err = errors.Warning("forg: mapping type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(errors.Warning("unsupported kind"))
+		break
 	}
 	return
 }
 
 func mapBasicTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
 	if ctx.Err() != nil {
-		err = errors.Warning("forg: map type to function document element code, failed").
+		err = errors.Warning("forg: mapping basic type to function document element code, failed").
 			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
 			WithCause(ctx.Err())
+		return
 	}
 	stmt := gcg.Statements()
 	switch typ.Name {
@@ -106,12 +114,81 @@ func mapBasicTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (c
 	return
 }
 
+func mapBuiltinTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping builtin type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	code = gcg.Statements().Token("documents.Ref(").Token(fmt.Sprintf("\"%s\",\"%s\"", typ.Path, typ.Name)).Symbol(")")
+	return
+}
+
+func mapIdentTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping ident type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	targetCode, targetCodeErr := MapTypeToFunctionElementCode(ctx, typ.Elements[0])
+	if targetCodeErr != nil {
+		err = errors.Warning("forg: mapping ident type to function element code failed").
+			WithMeta("name", typ.Name).WithMeta("path", typ.Path).
+			WithCause(targetCodeErr)
+		return
+	}
+	code = gcg.Statements().Token("documents.Ident(").Line().
+		Token(fmt.Sprintf("\"%s\",\"%s\"", typ.Path, typ.Name)).Symbol(",").Line().
+		Add(targetCode).Symbol(",").Line().
+		Symbol(")")
+	return
+}
+
+func mapInterfaceTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping interface type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	stmt := gcg.Statements()
+	stmt = stmt.Token("documents.Struct(").Token(fmt.Sprintf("\"%s\",\"%s\"", typ.Path, typ.Name)).Symbol(")")
+	title, hasTitle := typ.Annotations.Get("title")
+	if hasTitle {
+		stmt = stmt.Dot().Line().Token("SetTitle(").Token(fmt.Sprintf("\"%s\"", title)).Symbol(")")
+	}
+	description, hasDescription := typ.Annotations.Get("description")
+	if hasDescription {
+		stmt = stmt.Dot().Line().Token("SetDescription(").Token(fmt.Sprintf("\"%s\"", description)).Symbol(")")
+	}
+	_, hasDeprecated := typ.Annotations.Get("deprecated")
+	if hasDeprecated {
+		stmt = stmt.Dot().Line().Token("AsDeprecated()")
+	}
+	code = stmt
+	return
+}
+
 func mapPointerTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping pointer type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
 	code, err = MapTypeToFunctionElementCode(ctx, typ.Elements[0])
 	return
 }
 
 func mapStructTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping struct type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
 	if typ.ParadigmsPacked != nil {
 		typ = typ.ParadigmsPacked
 	}
@@ -137,95 +214,118 @@ func mapStructTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (
 		if name == "-" {
 			continue
 		}
-		elementCode, elementCodeErr := MapTypeToFunctionElementCode(ctx, field.Elements[0])
-		if elementCodeErr != nil {
-			err = errors.Warning("forg: map struct type to function element code failed").
+		fieldCode, fieldCodeErr := MapTypeToFunctionElementCode(ctx, field)
+		if fieldCodeErr != nil {
+			err = errors.Warning("forg: mapping struct type to function element code failed").
 				WithMeta("name", typ.Name).WithMeta("path", typ.Path).
-				WithMeta("field", field.Name).
-				WithCause(elementCodeErr)
+				WithMeta("field", typ.Name).
+				WithCause(fieldCodeErr)
 			return
 		}
-		elementStmt := elementCode.(*gcg.Statement)
-		fieldTitle, hasFieldTitle := field.Annotations.Get("title")
-		if hasFieldTitle {
-			elementStmt = elementStmt.Dot().Line().Token("SetTitle(").Token(fmt.Sprintf("\"%s\"", fieldTitle)).Symbol(")")
-		}
-		fieldDescription, hasFieldDescription := field.Annotations.Get("description")
-		if hasFieldDescription {
-			elementStmt = elementStmt.Dot().Line().Token("SetDescription(").Token(fmt.Sprintf("\"%s\"", fieldDescription)).Symbol(")")
-		}
-		_, hasFieldDeprecated := field.Annotations.Get("deprecated")
-		if hasFieldDeprecated {
-			elementStmt = elementStmt.Dot().Line().Token("AsDeprecated()")
-		}
-		// enum
-		fieldEnum, hasFieldEnum := field.Annotations["enum"]
-		if hasFieldEnum && fieldEnum != "" {
-			fieldEnums := strings.Split(fieldEnum, ",")
-			fieldEnumsCodeToken := ""
-			for _, enumValue := range fieldEnums {
-				fieldEnumsCodeToken = fieldEnumsCodeToken + `, "` + strings.TrimSpace(enumValue) + `"`
-			}
-			if fieldEnumsCodeToken != "" {
-				fieldEnumsCodeToken = fieldEnumsCodeToken[2:]
-				elementStmt = elementStmt.Dot().Line().Token("AddEnum").Symbol("(").Token(fieldEnumsCodeToken).Symbol(")")
-			}
-		}
-		// validation
-		fieldValidate, hasFieldValidate := field.Tags["validate"]
-		if hasFieldValidate && fieldValidate != "" {
-			fieldRequired := strings.Contains(fieldValidate, "required")
-			if fieldRequired {
-				elementStmt = elementStmt.Dot().Line().Token("AsRequired()")
-			}
-			fieldValidateMessage, hasFieldValidateMessage := field.Tags["validate-message"]
-			if !hasFieldValidateMessage {
-				fieldValidateMessage = field.Tags["message"]
-			}
-			fieldValidateMessageI18ns := make([]string, 0, 1)
-			validateMessageI18n, hasValidateMessageI18n := field.Annotations.Get("validate-message-i18n")
-			if hasValidateMessageI18n && validateMessageI18n != "" {
-				reader := bufio.NewReader(bytes.NewReader([]byte(validateMessageI18n)))
-				for {
-					line, _, readErr := reader.ReadLine()
-					if readErr != nil {
-						break
-					}
-					idx := bytes.IndexByte(line, ':')
-					if idx > 0 && idx < len(line) {
-						fieldValidateMessageI18ns = append(fieldValidateMessageI18ns, strings.TrimSpace(string(line[0:idx])))
-						fieldValidateMessageI18ns = append(fieldValidateMessageI18ns, strings.TrimSpace(string(line[idx+1:])))
-					}
-				}
-			}
-			fieldValidateMessageI18nsCodeToken := ""
-			for _, fieldValidateMessageI18n := range fieldValidateMessageI18ns {
-				fieldValidateMessageI18nsCodeToken = fieldValidateMessageI18nsCodeToken + `, "` + fieldValidateMessageI18n + `"`
-			}
-			if fieldValidateMessageI18nsCodeToken != "" {
-				fieldValidateMessageI18nsCodeToken = fieldValidateMessageI18nsCodeToken[2:]
-			}
-			elementStmt = elementStmt.Dot().Line().Token("SetValidation(").
-				Token("documents.NewElementValidation(").
-				Token(fmt.Sprintf("\"%s\"", fieldValidateMessage)).
-				Symbol(", ").
-				Token(fieldValidateMessageI18nsCodeToken).
-				Symbol(")").
-				Symbol(")")
-		}
-
 		stmt = stmt.Dot().Line().
 			Token("AddProperty(").Line().
 			Token(fmt.Sprintf("\"%s\"", name)).Symbol(",").Line().
-			Add(elementStmt).Symbol(",").Line().
+			Add(fieldCode).Symbol(",").Line().
 			Symbol(")")
 	}
+	code = stmt
+	return
+}
 
+func mapStructFieldTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping struct field type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	elementCode, elementCodeErr := MapTypeToFunctionElementCode(ctx, typ.Elements[0])
+	if elementCodeErr != nil {
+		err = errors.Warning("forg: mapping struct field type to function element code failed").
+			WithMeta("name", typ.Name).WithMeta("path", typ.Path).
+			WithMeta("field", typ.Name).
+			WithCause(elementCodeErr)
+		return
+	}
+	stmt := elementCode.(*gcg.Statement)
+	fieldTitle, hasFieldTitle := typ.Annotations.Get("title")
+	if hasFieldTitle {
+		stmt = stmt.Dot().Line().Token("SetTitle(").Token(fmt.Sprintf("\"%s\"", fieldTitle)).Symbol(")")
+	}
+	fieldDescription, hasFieldDescription := typ.Annotations.Get("description")
+	if hasFieldDescription {
+		stmt = stmt.Dot().Line().Token("SetDescription(").Token(fmt.Sprintf("\"%s\"", fieldDescription)).Symbol(")")
+	}
+	_, hasFieldDeprecated := typ.Annotations.Get("deprecated")
+	if hasFieldDeprecated {
+		stmt = stmt.Dot().Line().Token("AsDeprecated()")
+	}
+	// enum
+	fieldEnum, hasFieldEnum := typ.Annotations["enum"]
+	if hasFieldEnum && fieldEnum != "" {
+		fieldEnums := strings.Split(fieldEnum, ",")
+		fieldEnumsCodeToken := ""
+		for _, enumValue := range fieldEnums {
+			fieldEnumsCodeToken = fieldEnumsCodeToken + `, "` + strings.TrimSpace(enumValue) + `"`
+		}
+		if fieldEnumsCodeToken != "" {
+			fieldEnumsCodeToken = fieldEnumsCodeToken[2:]
+			stmt = stmt.Dot().Line().Token("AddEnum").Symbol("(").Token(fieldEnumsCodeToken).Symbol(")")
+		}
+	}
+	// validation
+	fieldValidate, hasFieldValidate := typ.Tags["validate"]
+	if hasFieldValidate && fieldValidate != "" {
+		fieldRequired := strings.Contains(fieldValidate, "required")
+		if fieldRequired {
+			stmt = stmt.Dot().Line().Token("AsRequired()")
+		}
+		fieldValidateMessage, hasFieldValidateMessage := typ.Tags["validate-message"]
+		if !hasFieldValidateMessage {
+			fieldValidateMessage = typ.Tags["message"]
+		}
+		fieldValidateMessageI18ns := make([]string, 0, 1)
+		validateMessageI18n, hasValidateMessageI18n := typ.Annotations.Get("validate-message-i18n")
+		if hasValidateMessageI18n && validateMessageI18n != "" {
+			reader := bufio.NewReader(bytes.NewReader([]byte(validateMessageI18n)))
+			for {
+				line, _, readErr := reader.ReadLine()
+				if readErr != nil {
+					break
+				}
+				idx := bytes.IndexByte(line, ':')
+				if idx > 0 && idx < len(line) {
+					fieldValidateMessageI18ns = append(fieldValidateMessageI18ns, strings.TrimSpace(string(line[0:idx])))
+					fieldValidateMessageI18ns = append(fieldValidateMessageI18ns, strings.TrimSpace(string(line[idx+1:])))
+				}
+			}
+		}
+		fieldValidateMessageI18nsCodeToken := ""
+		for _, fieldValidateMessageI18n := range fieldValidateMessageI18ns {
+			fieldValidateMessageI18nsCodeToken = fieldValidateMessageI18nsCodeToken + `, "` + fieldValidateMessageI18n + `"`
+		}
+		if fieldValidateMessageI18nsCodeToken != "" {
+			fieldValidateMessageI18nsCodeToken = fieldValidateMessageI18nsCodeToken[2:]
+		}
+		stmt = stmt.Dot().Line().Token("SetValidation(").
+			Token("documents.NewElementValidation(").
+			Token(fmt.Sprintf("\"%s\"", fieldValidateMessage)).
+			Symbol(", ").
+			Token(fieldValidateMessageI18nsCodeToken).
+			Symbol(")").
+			Symbol(")")
+	}
 	code = stmt
 	return
 }
 
 func mapArrayTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping array type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
 	element := typ.Elements[0]
 	name, isBasic := element.Basic()
 	if isBasic && (name == "byte" || name == "uint8") {
@@ -236,7 +336,7 @@ func mapArrayTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (c
 	}
 	elementCode, elementCodeErr := MapTypeToFunctionElementCode(ctx, element)
 	if elementCodeErr != nil {
-		err = errors.Warning("forg: map array type to function element code failed").
+		err = errors.Warning("forg: mapping array type to function element code failed").
 			WithMeta("name", typ.Name).WithMeta("path", typ.Path).
 			WithCause(elementCodeErr)
 		return
@@ -251,6 +351,88 @@ func mapArrayTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (c
 	if hasDescription {
 		stmt = stmt.Dot().Line().Token("SetDescription(").Token(fmt.Sprintf("\"%s\"", description)).Symbol(")")
 	}
+	_, hasDeprecated := typ.Annotations.Get("deprecated")
+	if hasDeprecated {
+		stmt = stmt.Dot().Line().Token("AsDeprecated()")
+	}
 	code = stmt
+	return
+}
+
+func mapMapTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping map type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	element := typ.Elements[1]
+	elementCode, elementCodeErr := MapTypeToFunctionElementCode(ctx, element)
+	if elementCodeErr != nil {
+		err = errors.Warning("forg: mapping map type to function element code failed").
+			WithMeta("name", typ.Name).WithMeta("path", typ.Path).
+			WithCause(elementCodeErr)
+		return
+	}
+	stmt := gcg.Statements()
+	stmt = stmt.Token("documents.Map(").Add(elementCode).Symbol(")")
+	title, hasTitle := typ.Annotations.Get("title")
+	if hasTitle {
+		stmt = stmt.Dot().Line().Token("SetTitle(").Token(fmt.Sprintf("\"%s\"", title)).Symbol(")")
+	}
+	description, hasDescription := typ.Annotations.Get("description")
+	if hasDescription {
+		stmt = stmt.Dot().Line().Token("SetDescription(").Token(fmt.Sprintf("\"%s\"", description)).Symbol(")")
+	}
+	_, hasDeprecated := typ.Annotations.Get("deprecated")
+	if hasDeprecated {
+		stmt = stmt.Dot().Line().Token("AsDeprecated()")
+	}
+	code = stmt
+	return
+}
+
+func mapAnyTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping any type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	code = gcg.Statements().Token("documents.Any()")
+	return
+}
+
+func mapParadigmTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping paradigm type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	code, err = MapTypeToFunctionElementCode(ctx, typ.ParadigmsPacked)
+	if err != nil {
+		err = errors.Warning("forg: mapping paradigm type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(err)
+		return
+	}
+	return
+}
+
+func mapParadigmElementTypeToFunctionElementCode(ctx context.Context, typ *module.Type) (code gcg.Code, err error) {
+	if ctx.Err() != nil {
+		err = errors.Warning("forg: mapping paradigm element type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(ctx.Err())
+		return
+	}
+	code, err = MapTypeToFunctionElementCode(ctx, typ.ParadigmsPacked)
+	if err != nil {
+		err = errors.Warning("forg: mapping paradigm element type to function document element code, failed").
+			WithMeta("path", typ.Path).WithMeta("name", typ.Name).WithMeta("kind", typ.Kind.String()).
+			WithCause(err)
+		return
+	}
 	return
 }
